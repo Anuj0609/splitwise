3debbf867b7584238ea382b037a3fbe8f27a882f
Anuj0609 <anujshrivastava982@gmail.com>
2024-08-16 09:55:28 +0530

    displayExpense

--- src/components/DatepickerWithIcon.tsx
+++ src/components/DatepickerWithIcon.tsx
@@ -4,19 +4,19 @@ import "react-datepicker/dist/react-datepicker.css";
 import { FaCalendarAlt } from "react-icons/fa";
 
 const DatepickerWithIcon = () => {
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   return (
     <div className="relative inline-block">
       <DatePicker
         selected={selectedDate}
         onChange={(date) => setSelectedDate(date)}
-        placeholderText="Select a date"
+        placeholderText={new Date().toISOString()}
         dateFormat="dd/MM/yyyy"
         className="w-full pl-3 pr-10 py-1 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
       />
       <span className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none text-gray-400">
-        <FaCalendarAlt/>
+        <FaCalendarAlt />
       </span>
     </div>
   );
